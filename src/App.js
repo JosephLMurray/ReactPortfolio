a9fe4cd996ac4098c774017ea4b59b3ee39cfffd
Joseph Murray <josephl.murrayjr@gmail.com>
2022-06-01 19:04:09 -0400

    added base styling and created assets folder with fonts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
-import Layout from "./components/Layout";
-import { Routes, Route } from "react-router-dom";
+import './App.scss'
+import Layout from './components/Layout'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}></Route>
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App
